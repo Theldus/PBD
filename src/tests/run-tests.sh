13c0fec9bdbe6445e20a2c7b89605830a138871f
Theldus <davidsondfgl@gmail.com>
2019-11-18 09:47:48 -0300

    Improved tests: Added Expected Output

--- src/tests/run-tests.sh
+++ src/tests/run-tests.sh
@@ -24,19 +24,57 @@
 # SOFTWARE.
 #
 
+# Fancy colors =)
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+# PBD Folder
 PBD_FOLDER=$(readlink -f ../)
 
-#Run iterative and recursive tests
+echo -n "Tests..."
+
+# Run iterative and recursive tests
 {
-	$PBD_FOLDER/pbd test func1 &&\
+	$PBD_FOLDER/pbd test func1     > test_func1_out &&\
 	$PBD_FOLDER/pbd test factorial
 } &> /dev/null
 
 if [ $? -eq 0 ]
 then
-	echo "Tests [PASSED]"
-	exit 0
-else
-	echo "Tests [NOT PASSED]"
-	exit 1
+	# Note:
+	#
+	# If all tests were able to run and finish without errors, its 'almost'
+	# sure that the output will be ok too, nevertheless, we need to ensure.
+	#
+	#
+	#
+	if cmp -s "test_func1_expected" "test_func1_out"
+	then
+		echo -e " [${GREEN}PASSED${NC}]"
+
+		if [ -x "$(command -v valgrind)" ]
+		then
+			echo -n "Valgrind tests..."
+
+			{
+				valgrind --leak-check=full --error-exitcode=1\
+					$PBD_FOLDER/pbd test func1 &&\
+				valgrind --leak-check=full --error-exitcode=1\
+					$PBD_FOLDER/pbd test factorial
+			} &> /dev/null
+
+			if [ $? -eq 0 ]
+			then
+				echo -e " [${GREEN}PASSED${NC}]"
+			else
+				echo -e " [${RED}NOT PASSED${NC}]"
+				exit 1
+			fi
+		fi
+		exit 0
+	else
+		echo -e " [${RED}NOT PASSED${NC}]"
+		exit 1
+	fi
 fi
